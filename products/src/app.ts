96f33cdba725a5ef637937c579ba00c58382b82a
philz-stizles <theophilusighalo@gmail.com>
2021-07-27 23:54:34 +0100

    init product service

---
+++ products/src/app.ts
@@ -0,0 +1,26 @@
+import express from 'express'
+import 'express-async-errors'
+import cookieSession from 'cookie-session'
+import { errorHandler, NotFoundError } from '@devdezyn/common'
+
+const app = express()
+
+app.set('trust proxy', 1)
+
+app.use(express.json())
+
+app.use(
+  cookieSession({
+    // name: 'session',
+    signed: false,
+    secure: process.env.NODE_ENV !== 'test',
+  })
+)
+
+app.all('*', async () => {
+  throw new NotFoundError()
+})
+
+app.use(errorHandler)
+
+export default app
